4387582380e2e3dbfcbc02d8cb575f3a890115fc
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-05-07 17:49:44 +0900

    Update TodoList.jsx

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,101 +1,70 @@
 import List from '@mui/material/List';
-import TodoItem from './TodoItem';
+import Todo from './Todo';
 import TodoForm from './TodoForm';
-import { useState } from 'react';
+import { Box, Typography } from '@mui/material';
+import { useState, useEffect } from "react";
 
-const initialTodos = [
-    {id: 1, text: "walk the dog", completed: false},
-    {id: 3, text: "walk the cat", completed: false},
-    {id: 4, text: "walk the fish", completed: true},
-    {id: 5, text: "walk the chickens", completed: true},
-];
+const getInitialData = () => {
+    const data = JSON.parse(localStorage.getItem("todos"));
+    if (!data) return [];
+    return data;
+};
 
 export default function TodoList() {
-    const [todos, setTodos] = useState(initialTodos);
-    
-    const removeTodo = (id) => {
+    const [todos, setTodos] = useState(getInitialData);
+
+    // 부수적인 효과 실행 - 로컬스토리지에 todos를 문자열로 저장 
+    // 새로고침했을 때, 로컬스토리지 내용을 불러와 데이터를 보존 가능   
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    const removeTodo = ((id) => {
         setTodos((prevTodos) => {
             return prevTodos.filter((t) => t.id !== id);
         })
-    }
-    
-    const toggleTodo = (id) => {
+    })
+
+    const toggleTodo = ((id) => {
         setTodos((prevTodos) => {
             return prevTodos.map((todo) => {
                 if (todo.id === id) {
-                    return {...todo, completed: !todo.completed};
+                    return { ...todo, completed: !todo.completed };
                 } else {
                     return todo;
                 }
             })
         })
-    }
+    })
 
     const addTodo = (text) => {
-        setTodos((prevTodos) => {
-            return [...prevTodos, {text: text, id: 8, completed: false}];
+        setTodos(prevTodos => {
+            return [...prevTodos, { text: text, id: crypto.randomUUID(), completed: false }]
         })
     }
 
     return (
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-            {todos.map((todo) => (
-                <TodoItem todo = {todo} key = {todo.id} removeTodo={() => removeTodo(todo.id)} toggleTodo = {() => toggleTodo(todo.id)} />
-            )
-            )}
-            <TodoForm addTodo = {addTodo}/>
-        </List>
+        <Box
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                flexDirection: "column", 
+                alignItems: 'center',
+                m: 3,
+            }}
+        >
+            <Typography variant="h2" component="h1" sx={{ flexGrow: 1 }}>
+                Todos
+            </Typography>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+                {todos.map((todo) => {
+                    return (
+                        <Todo key={todo.id} todo={todo} removeTodo={() => removeTodo(todo.id)} toggleTodo={() => toggleTodo(todo.id)} />
+                    )
+                })}
+                <TodoForm addTodo={addTodo} />
+            </List>
+        </Box>
     )
 }
 
-
-
-// export default function CheckboxList() {
-//   const [checked, setChecked] = React.useState([0]);
-
-//   const handleToggle = (value) => () => {
-//     const currentIndex = checked.indexOf(value);
-//     const newChecked = [...checked];
-
-//     if (currentIndex === -1) {
-//       newChecked.push(value);
-//     } else {
-//       newChecked.splice(currentIndex, 1);
-//     }
-
-//     setChecked(newChecked);
-//   };
-
-//   return (
-//     <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-//       {[0, 1, 2, 3].map((value) => {
-//         const labelId = `checkbox-list-label-${value}`;
-
-//         return (
-//           <ListItem
-//             key={value}
-//             secondaryAction={
-//               <IconButton edge="end" aria-label="comments">
-//                 <CommentIcon />
-//               </IconButton>
-//             }
-//             disablePadding
-//           >
-//             <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-//               <ListItemIcon>
-//                 <Checkbox
-//                   edge="start"
-//                   checked={checked.indexOf(value) !== -1}
-//                   tabIndex={-1}
-//                   disableRipple
-//                   inputProps={{ 'aria-labelledby': labelId }}
-//                 />
-//               </ListItemIcon>
-//               <ListItemText id={labelId} primary={`Line item ${value + 1}`} />
-//             </ListItemButton>
-//           </ListItem>
-//         );
-//       })}
-//     </List>
-//   );
-// }
