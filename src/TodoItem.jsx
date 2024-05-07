93b73b781d2eb43ccc7a321284e3e78a7fa07dab
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-05-07 17:48:26 +0900

    Update TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -4,34 +4,35 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 
-export default function TodoItem ({todo, removeTodo, toggleTodo}) {
+export default function Todo({ todo, removeTodo, toggleTodo }) {
     const labelId = `checkbox-list-label-${todo.id}`;
 
-                return (
-                  <ListItem
-                    secondaryAction={
-                      <IconButton edge="end" aria-label="comments" onClick={removeTodo}>
-                        <CommentIcon />
-                      </IconButton>
-                    }
-                    disablePadding
-                  >
-                    <ListItemButton role={undefined} dense>
-                      <ListItemIcon>
-                        <Checkbox
-                          edge="start"
-                          checked={todo.completed}
-                          tabIndex={-1}
-                          disableRipple
-                          inputProps={{ 'aria-labelledby': labelId }}
-                          onChange={toggleTodo}
-                        />
-                      </ListItemIcon>
-                      <ListItemText id={labelId} primary={todo.text} />
-                    </ListItemButton>
-                  </ListItem>
-                );
+    return (
+        <ListItem
+            key={todo.id}
+            secondaryAction={
+                <IconButton edge="end" aria-label="comments" onClick={removeTodo}>
+                    <DeleteIcon />
+                </IconButton>
+            }
+            disablePadding
+        >
+            <ListItemButton role={undefined} dense>
+                <ListItemIcon>
+                    <Checkbox
+                        edge="start"
+                        checked={todo.completed}
+                        tabIndex={-1}
+                        disableRipple
+                        inputProps={{ 'aria-labelledby': labelId }}
+                        onChange={toggleTodo}
+                    />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={todo.text} />
+            </ListItemButton>
+        </ListItem>
+    );
 }
